5ecffc884001afc9f32f28fc42e273cf6651524e
zordrek <zordrek@gmail.com>
2023-01-21 22:12:34 +0500

    as

--- src/CardFromDBnew.java
+++ src/CardFromDBnew.java
@@ -4,45 +4,56 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 public class CardFromDBnew {
 
     public static void main(String[] args) {
-        int pin = 3203;
-        Connection connection = null;
-        try {
-            // Step 1: Load the JDBC driver.
-            Class.forName("org.postgresql.Driver");
+        //CardFromDBsec cfdb = new CardFromDBsec();
+        //cfdb.CardFromDBsec();
+        CardFromDBsec.CardFromDBsec();
+    }
+}
+    class CardFromDBsec {
 
-            // Step 2: Establish the connection to the database.
-            String url = "jdbc:postgresql://localhost:5432/java_one";
-            connection = DriverManager.getConnection(url, "postgres", "Qwerty78");
+        public static void CardFromDBsec() {
+            int pin_read = 2379;
+            Connection connection = null;
+            try {
+                // Step 1: Load the JDBC driver.
+                Class.forName("org.postgresql.Driver");
 
-            // Step 3: Create a statement.
-            Statement statement = connection.createStatement();
+                // Step 2: Establish the connection to the database.
+                String url = "jdbc:postgresql://localhost:5432/java_one";
+                connection = DriverManager.getConnection(url, "postgres", "Qwerty78");
 
-            // Step 4: Execute a query.
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM card WHERE pin=" + pin);
+                // Step 3: Create a statement.
+                Statement statement = connection.createStatement();
 
-            // Step 5: Process the results.
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3)+ " " + resultSet.getString(4)+ " " + resultSet.getString(5));
-            }
+                // Step 4: Execute a query.
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM card WHERE pin=" + pin_read);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            // Step 6: Close the connection.
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                // Step 5: Process the results.
+                while (resultSet.next()) {
+                    String id = resultSet.getString(1);
+                    String number = resultSet.getString(2);
+                    String owner = resultSet.getString(3);
+                    String balance = resultSet.getString(4);
+                    String pin = resultSet.getString(5);
+                    System.out.println(id + " " + number + " " + owner + " " + balance + " " + pin);
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                // Step 6: Close the connection.
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
     }
-}
